6a5dfaf8672b1a3427f70c11b9fdebf8c25f4278
Giantpro1 <shakiruolawale597@gmail.com>
2023-01-11 03:02:34 -0800

    app styling

---
+++ lib/utils/app_style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+Color primary = const Color(0xFF687daf);
+ 
+ class Styles {
+   static Color primaryColor = primary;
+   static Color textColor = const Color(0xFF3b3b3b);
+   static Color bgColor = const Color(0xFFEEEDF2);
+   static Color orangeColor = const Color(0xFF526799);
+   static TextStyle textStyle = TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
+   static TextStyle headLineStyle1 = TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.bold);
+   static TextStyle headLineStyle2 = TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
+   static TextStyle headLineStyle3 = const TextStyle(fontSize: 17, color: Color(0xFF9E9E9E), fontWeight: FontWeight.w500);
+   static TextStyle headLineStyle4 = const TextStyle(fontSize: 14, color: Color(0xFF9E9E9E), fontWeight: FontWeight.w500);
+  
+ } 
